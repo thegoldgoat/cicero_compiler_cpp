e744ee0b7ff2fd1d99b6d2daf84e704f484240df
Andrea Somaini <andrea.somaini@mail.polimi.it>
2023-07-16 20:54:38 +0200

    Introduced PlaceholderRemover pass
    This pass removes placeholder from the IR, which means that now we shoud
    have an IR which is completely ready for easy codegen

--- src/Passes.cpp
+++ src/Passes.cpp
@@ -37,3 +37,52 @@ void cicero_compiler::dialect::SplitOp::getCanonicalizationPatterns(
     mlir::RewritePatternSet &results, mlir::MLIRContext *context) {
     results.add<FlattenSplit>(context);
 }
+
+struct PlaceholderRemover : public mlir::OpRewritePattern<PlaceholderOp> {
+    PlaceholderRemover(mlir::MLIRContext *context)
+        : OpRewritePattern(context, /*benefit=*/1000) {}
+
+    mlir::LogicalResult
+    matchAndRewrite(PlaceholderOp op,
+                    mlir::PatternRewriter &rewriter) const override {
+
+        auto nextOp = op.getOperation()->getNextNode();
+
+        if (!nextOp) {
+            op.emitError("PlaceholderOp must be followed by some other "
+                         "operation??? Or should we just remove it and assign "
+                         "my symbol to operation before me?");
+            return mlir::failure();
+        }
+
+        auto nextOpSymbol = nextOp->getAttrOfType<mlir::StringAttr>(
+            mlir::SymbolTable::getSymbolAttrName());
+
+        if (!nextOpSymbol) {
+            mlir::SymbolTable::setSymbolName(nextOp, op.getName());
+            rewriter.eraseOp(op);
+            return mlir::success();
+        }
+
+        for (auto walker = op.getOperation(); walker;
+             walker = walker->getParentOp()) {
+
+            if (mlir::SymbolTable::replaceAllSymbolUses(op.getOperation(),
+                                                        nextOpSymbol, walker)
+                    .failed()) {
+                throw std::runtime_error(
+                    "During PlaceholderRemover, failed to replace all symbol "
+                    "uses of the placeholder symbol with the next operation's "
+                    "symbol");
+            }
+        }
+
+        rewriter.eraseOp(op);
+        return mlir::success();
+    }
+};
+
+void cicero_compiler::dialect::PlaceholderOp::getCanonicalizationPatterns(
+    mlir::RewritePatternSet &results, mlir::MLIRContext *context) {
+    results.add<PlaceholderRemover>(context);
+}
