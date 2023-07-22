4d3c6f0150e5dcf5545177b346ee4fcd25fa6382
Andrea Somaini <andrea.somaini@mail.polimi.it>
2023-07-22 12:18:44 +0200

    Completed testing environment
    Added simulator "SoftwareCicero" as submodule
    Compile test regexes and run them

--- tests/data/generate_csv.py
+++ tests/data/generate_csv.py
@@ -2,18 +2,25 @@ import csv
 import os
 import re
 import sys
+import subprocess
 
-if len(sys.argv) != 2:
-    print('Usage: python3 generate_csv.py <output_file>')
+if len(sys.argv) != 4:
+    print('Usage: python3 generate_csv.py <output_file> <compiled_regexes_path> <path_to_compiler>')
     sys.exit(1)
 
+results_path = sys.argv[1]
+compiled_regexes_path = sys.argv[2]
+compiler_path = sys.argv[3]
+
+# create the compiled regexes directory if it doesn't exist
+if not os.path.exists(compiled_regexes_path):
+    os.makedirs(compiled_regexes_path)
 
 # Get the absolute path of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 regexes_path = os.path.join(script_dir, 'regexes.txt')
 inputs_path = os.path.join(script_dir, 'inputs.txt')
-results_path = sys.argv[1]
 
 # Open file1 and read all the regexes into a list
 with open(regexes_path, 'r') as f:
@@ -27,11 +34,19 @@ with open(inputs_path, 'r') as f:
 results = []
 
 # Loop through each regex in the regex list
-for regex in regex_list:
+for regex_index, regex in enumerate(regex_list):
+    regex_compiled_path = os.path.join(
+        compiled_regexes_path, str(regex_index) + '.txt')
+    # Compile the regex using the compiler
+    result = subprocess.run([compiler_path, "--regex", regex, "-o",
+                            regex_compiled_path, "--emit=compiled", "--binary-format=hex"])
+    if result.returncode != 0:
+        print('Error compiling regex: ' + regex)
+        sys.exit(1)
     for input_str in input_list:
         # Use the `re` module to match the regex with the input
         match = re.search(regex, input_str)
-        results.append((regex, input_str, 1 if match else 0))
+        results.append((regex_compiled_path, input_str, 1 if match else 0))
 
 # Open a output file for writing and write the results into CSV format
 with open(results_path, 'w', newline='') as f:
---
+++ tests/src/run_on_simulator.cpp
@@ -0,0 +1,53 @@
+#include "CSVParser.h"
+#include "CiceroMulti.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    auto input = parse_csv(REGEX_CSV_PATH);
+    Cicero::CiceroMulti cicero;
+
+    string lastProgram;
+    auto it = input.begin();
+    ++it; // skip first line of CSV that contains headers
+    for (; it != input.end(); ++it) {
+        auto row = *it;
+        // Assert row contains 3 elements
+        if (row.size() != 3) {
+            cerr << "Test failed, row from CSV does not have 3 element, has "
+                 << row.size() << endl;
+            for (auto element : row) {
+                cerr << element << endl;
+            }
+            assert(false);
+        }
+
+        string program = row[0];
+        string input = row[1];
+        string expectedAsString = row[2];
+
+        if (expectedAsString != "1" && expectedAsString != "0") {
+            cerr << "Test failed, expected value is not 1 or 0, but is "
+                 << expectedAsString << endl;
+            assert(false);
+        }
+
+        bool expected = expectedAsString == "1";
+
+        if (program != lastProgram) {
+            cicero.setProgram(program.c_str());
+            lastProgram = program;
+        }
+
+        bool actual = cicero.match(input);
+
+        if (actual != expected) {
+            cerr << "Test failed: " << program << " " << input << " "
+                 << expectedAsString << endl;
+            assert(false);
+        }
+    }
+    return 0;
+}
